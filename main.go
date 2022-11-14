c8a074c5fdfba091a6414d442410cf25e22ec675
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-11-14 13:28:15 -0800

    starting http

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 )
 
+
 func main() {
-	// Hello world, the web server
-
-	Handler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, "+req.RemoteAddr+"")
-		log.Println(req.RemoteAddr)
-	}
-
-	http.HandleFunc("/", Handler)
-  log.Println("http://localhost:8000/")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// Main Page
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+  
+	// Serve http
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
