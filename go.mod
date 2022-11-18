3af1b6ca5708f595d5953bd8f7cf6ab528cd8a30
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-11-18 08:30:06 -0800

    update gitignore

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NalinPlad/ip-war.git
 
 go 1.18
+
+require github.com/mattn/go-sqlite3 v1.14.16 // indirect
--- main.go
+++ main.go
@@ -4,14 +4,46 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"net"
+
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	log.Println(fmt.Sprintf("registering %s", r.RemoteAddr))
+	// Get IP
+	var ip,_,err = net.SplitHostPort(r.RemoteAddr)
+	
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	// Get lbname
+	var lbname = r.URL.Query().Get("name")
+
+	// Open DB
+	db, err := sql.Open("sqlite3", "./IP_Leaderboard.db")
+	
+	if err != nil {
+			log.Fatal(err)
+	}
+  
+	defer db.Close()
+
+	var sts = fmt.Sprintf("INSERT OVERWRITE TABLE address(IP, lbname) VALUES('%s','%s');", ip, lbname)	
+
+	_, err = db.Exec(sts)
+
+	if err != nil {
+			log.Fatal(err)
+	}
+	
+	log.Println(fmt.Sprintf("registered %s for %s", ip, lbname))
 }
 
 func main() {
+
 	// Main Page
 	http.HandleFunc("/play", Handle)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
