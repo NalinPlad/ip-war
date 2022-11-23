5daa8193cf11b2a59c623407e9ec6bb8ce583ed7
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2022-11-22 16:43:05 -0800

    mvp

--- public/index.js
+++ public/index.js
@@ -3,4 +3,40 @@
  *  lmk what cool ways you guys hack my little project
  */
 
+var getJSON = function(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+};
 
+const leaderBoard = document.getElementById("leaders");
+
+getJSON("data", (s, data) => {
+  if(s !== null){
+    console.log(`Error: ${s}`)
+  } else {
+    console.log(data)
+    data.forEach(address => {
+      const listItem = document.createElement('li');
+      listItem.appendChild(document.createTextNode(`${address[1]} - ${address[0]}`));
+      leaderBoard.appendChild(listItem);
+    })
+    
+    document.getElementById("loading").remove();
+  }
+})
+
+const registerName = document.getElementById("name");
+
+function register() {
+  window.location.assign("play?name="+registerName.value)
+}
